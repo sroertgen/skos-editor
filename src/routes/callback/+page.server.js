956346e56463a3b59d93296c9b91620b366028f3
@s.roertgen <sroertgen@gmail.com>
2023-11-16 12:03:12 +0100

    Update packages

--- src/routes/callback/+page.server.js
+++ src/routes/callback/+page.server.js
@@ -1,26 +1,26 @@
-import { PUBLIC_CLIENT_ID, PUBLIC_CLIENT_SECRET} from '$env/static/public';
+import { CLIENT_ID, CLIENT_SECRET } from '$env/static/private';
 
 const accessTokenURL = 'https://github.com/login/oauth/access_token';
 const userURL = 'https://api.github.com/user'
 
-export async function load({url}) {
-	const code = url.searchParams.get('code');
-	const accessToken = await getAccessToken(code);
+export async function load({ url }) {
+  const code = url.searchParams.get('code');
+  const accessToken = await getAccessToken(code);
   const user = await getUser(accessToken)
   const repos = await getRepos(accessToken, user)
-	return {user, repos, accessToken};
+  return { user, repos, accessToken };
 }
 
 function getAccessToken(code) {
-	return fetch(accessTokenURL, {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
-		body: JSON.stringify({
-			code: code,
-			client_id: PUBLIC_CLIENT_ID,
-			client_secret: PUBLIC_CLIENT_SECRET
-		})
-	}).then(r => r.json().then(r => r.access_token))
+  return fetch(accessTokenURL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+    body: JSON.stringify({
+      code: code,
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET
+    })
+  }).then(r => r.json().then(r => r.access_token))
 }
 
 function getUser(accessToken) {
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 // svelte.config.js
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import path from 'path';
 import preprocess from "svelte-preprocess";
 
@@ -14,16 +14,12 @@ export default {
     adapter: adapter({
       // default options are shown. On some platforms
       // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false
     }),
     alias: {
-      '$components':  path.resolve('./src/lib/components')
+      '$components': path.resolve('./src/lib/components')
     },
   },
   optimizeDeps: {
     exclude: ["codemirror", "@codemirror/language-javascript" /* ... */],
-},
+  },
 };
